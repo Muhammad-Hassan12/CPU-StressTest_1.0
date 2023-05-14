f2ba438fac19fb289ac448b7b65456250a64d8bc
S.M Hassan <118466320+Muhammad-Hassan12@users.noreply.github.com>
2023-05-15 00:27:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import time
 
-# The number of iterations to perform
 ITERATIONS = 100000000
 
 def calculate_pi():
@@ -13,11 +12,9 @@ def calculate_pi():
 
 start_time = time.time()
 
-# Perform the calculation in a loop
 while True:
     calculate_pi()
 
-    # Check if the desired time has elapsed
     elapsed_time = time.time() - start_time
-    if elapsed_time >= 60:  # Change the time limit as needed
+    if elapsed_time >= 60:
         break
